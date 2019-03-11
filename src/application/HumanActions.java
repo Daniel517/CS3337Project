a9238854c72d2174a1aad2e31cadd1375f7ff319
DanielGMagallanes <47930565+DanielGMagallanes@users.noreply.github.com>
2019-03-11 02:24:31 -0700

    basic classes for project
    These are just the out line of the classes as of now

---
+++ src/application/HumanActions.java
@@ -0,0 +1,15 @@
+package application;
+
+public interface HumanActions {
+	
+	String Pet();
+	String Hit();
+	String Feed();
+	String Groom();
+	String Cary();
+	String Walk();
+	String Report( );
+	String Discipline();
+	String Rest();
+
+}
