c1c27c6f6ffc81215483010ad2c9ea756fdd8280
DanielGMagallanes <dgmdragon@gmail.com>
2019-03-11 15:22:17 -0700

    general classes

---
+++ src/application/AnimalActions.java
@@ -0,0 +1,16 @@
+package application;
+
+public interface AnimalActions {
+	
+	String Fly();
+	String Catch();
+	String Speak();
+	String Chase();
+	String Flee();
+	String Bite();
+	String Play();
+	String Fetch();
+	String Lick();
+	String Rest();
+
+}
